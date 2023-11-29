c0e55774443c97904dbe6e5e9a7eb929b0065acf
Joe Nadherny <joenadherny@gmail.com>
2023-11-29 12:18:07 -0700

    More changes to everything

--- index.js
+++ index.js
@@ -1,70 +1,4 @@
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-// const {Circle, Square, Triangle}= require('./lib/shapes');
 
-
-// const questions = [
-//     {
-//         name: 'text',
-//         type: 'input',
-//         message: 'Enter 1-3 letters for your logo text:',
-
-//     },
-//     {
-//         name: 'textColor',
-//         type: 'input',
-//         message: 'Enter the color for your text:',  
-//     },
-//     {
-//     name: 'shape',
-//     type: 'input',
-//     message: 'Select shape options:',
-//     choices: ['triangle', 'circle', 'square']
-//     },
-//     {
-//         name: 'shapeColor',
-//         type: 'input',
-//         message: 'Enter color for your shape:',
-//     }
-// ];
-// async function generateAndWriteSVG(answers) {
-//     let shape;
-  
-//     // Create the corresponding shape object based on the user's selection
-//     switch (answers.shape.toLowerCase()) {
-//       case 'triangle':
-//         shape = new Triangle(answers.text, answers.textColor, answers.shapeColor);
-//         break;
-//       case 'circle':
-//         shape = new Circle(answers.text, answers.textColor, answers.shapeColor);
-//         break;
-//       case 'square':
-//         shape = new Square(answers.text, answers.textColor, answers.shapeColor);
-//         break;
-//       default:
-//         console.log('Invalid shape selection');
-//         return;
-//     }
-  
-//     const svgCode = shape.generateSVG();
-  
-//     // Save the SVG file in the 'examples' folder
-//     fs.writeFileSync('examples/logo.svg', svgCode);
-  
-//     console.log('Generated examples/logo.svg');
-//   }
-  
-
-// async function promptUser() {
-//   const answers = await inquirer.prompt(questions);
-
-  
-//   await generateAndWriteSVG(answers);
-// }
-
-
-// promptUser();
-// index.js
 
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -95,41 +29,54 @@ const questions = [
  
 ];
 
-async function generateAndWriteSVG(answers) {
-  let shape;
-
-  // Create the corresponding shape object based on the user's selection
-  switch (answers.shape.toLowerCase()) {
-    case 'triangle':
-      shape = new Triangle(Number(answers.sideLength), answers.textColor, answers.shapeColor);
-      break;
-    case 'circle':
-      shape = new Circle(Number(answers.sideLength), answers.textColor, answers.shapeColor);
-      break;
-    case 'square':
-      shape = new Square(Number(answers.sideLength), answers.textColor, answers.shapeColor);
-      break;
-    default:
-      console.log('Invalid shape selection');
-      return;
-  }
-
-  const svgCode = shape.render();
-
-  // Save the SVG content to an SVG file in the 'examples' folder
-  fs.writeFileSync('examples/logo.svg', svgCode);
-
-  console.log('Generated examples/logo.svg');
+function init(){
+    inquirer.prompt(questions)
+    .then ((data)=>{
+        if(data.text.length > 3 || data.text.length < 1){
+            console.log ('Please write only 3 letter maximum');
+            return init();
+        }
+        else{
+            writeAsFile(data);
+        }
+    })
 }
-async function promptUser() {
-  const answers = await inquirer.prompt(questions);
+const path = require('path');
 
+function writeAsFile(data) {
+  const svg = generateSVG(data);
+  const filePath = path.join(__dirname, 'examples', 'logo.svg');
+
+  fs.writeFile(filePath, svg, (err) => {
+    if (err) {
+      console.error('Error writing SVG file:', err);
+    } else {
+      console.log('Generated logo.svg');
+    }
+  });
+}
+
+function generateSVG(data) {
+    let shape;
   
-  await generateAndWriteSVG(answers);
-}
-
-
-promptUser();
+    if (data.shape.toLowerCase() === 'circle') {
+      shape = new Circle(data.text, data.textColor, data.shapeColor, 150, 100, 80);
+    } else if (data.shape.toLowerCase() === 'square') {
+      shape = new Square(data.text, data.textColor, data.shapeColor);
+    } else if (data.shape.toLowerCase() === 'triangle') {
+      shape = new Triangle(data.text, data.textColor, data.shapeColor);
+    } else {
+      console.log('Invalid shape selection');
+      return '';
+    }
+  
+    return `
+      <svg version='1.1' width='300' height='200' xmlns='https://www.w3.org/2000/svg'>
+        ${shape.render()}
+        ${shape.renderText()}
+      </svg>`;
+  }
+  init();
 
 
 
